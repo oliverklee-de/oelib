220bd173366309d0e545ce9812689deb0d06c53a
Oliver Klee <typo3-coding@oliverklee.de>
2026-03-05 22:33:17 +0100

    [TASK] Convert all testing framework tests to functional tests

--- Tests/Functional/Testing/Fixtures/TestingCleanup.php
+++ Tests/Functional/Testing/Fixtures/TestingCleanup.php
@@ -2,7 +2,7 @@
 
 declare(strict_types=1);
 
-namespace OliverKlee\Oelib\Tests\Unit\Testing\Fixtures;
+namespace OliverKlee\Oelib\Tests\Functional\Testing\Fixtures;
 
 /**
  * Testing class for the testing framework cleanup hook.
