e0cafd592ad2cc3b4bfb3af4b94e731570990704
Oliver Klee <typo3-coding@oliverklee.de>
2026-05-13 15:36:26 +0200

    [TASK] Drop `FrontEndUserMapper::findByUserName`
    Fixes #1992

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -13,6 +13,7 @@ This project adheres to [Semantic Versioning](https://semver.org/).
 
 ### Removed
 
+- Drop `FrontEndUserMapper::findByUserName()` (#2377)
 - Drop explicit support for readonly models (#2376)
 - Drop the `FrontEndUserGroup` model and mapper (#2375)
 - Drop the `BackEndUser` model and mapper (#2374)
--- Classes/Mapper/FrontEndUserMapper.php
+++ Classes/Mapper/FrontEndUserMapper.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 
 namespace OliverKlee\Oelib\Mapper;
 
-use OliverKlee\Oelib\Exception\NotFoundException;
 use OliverKlee\Oelib\Model\FrontEndUser;
 
 /**
@@ -15,24 +14,4 @@ class FrontEndUserMapper extends AbstractDataMapper
     protected $tableName = 'fe_users';
 
     protected $modelClassName = FrontEndUser::class;
-
-    // @deprecated #1992 will be removed in version 7.0
-    protected $additionalKeys = ['username'];
-
-    /**
-     * Finds a front-end user by username. Hidden user records will be
-     * retrieved as well.
-     *
-     * @param non-empty-string $username username, case-insensitive
-     *
-     * @return FrontEndUser model of the front-end user with the provided username
-     *
-     * @throws NotFoundException if there is no front-end user with the provided username in the database
-     *
-     * @deprecated #1992 will be removed in version 7.0
-     */
-    public function findByUserName(string $username): FrontEndUser
-    {
-        return $this->findOneByKey('username', $username);
-    }
 }
